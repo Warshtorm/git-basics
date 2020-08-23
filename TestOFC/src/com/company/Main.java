d596b06cc042ef9fb5f968519300e3233d02ecd5
Yura Bondarev <jary@inbox.ru>
2020-08-23 16:52:21 +0500

    test ofc 1.0

--- TestOFC/src/com/company/Main.java
+++ TestOFC/src/com/company/Main.java
@@ -3,14 +3,37 @@ package com.company;
 import javax.swing.*;
 import java.awt.*;
 import java.lang.reflect.Array;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Array[] array = new Array[100];
+        boolean response;  // переменная для сравнения ответа
+        String[] array = new String[100];  //массив для вопросов
+        Scanner scan = new Scanner(System.in);  // ввод в консоль ответа
 
-        array[0] = "akakjdj";
-        System.out.println("Длина струи пожарного ствола СПРК-8Б 25 метров.");
+
+
+        array[0] = "Длина струи пожарного ствола СПРК-8Б 25 метров.";
+        response = true;
+        System.out.println(array[0]);
+
+        array[1] = "Длина струи пожарного ствола СПРК-8Б 35 метров.";
+        response = false;
+        System.out.println(array[1]);
+        array[2] = "";
+        response = false;
+        System.out.println(array[2]);
+        array[3] = "";
+        response = true;
+        System.out.println(array[3]);
+        array[4] = "";
+        response = false;
+        System.out.println(array[4]);
+        array[5] = "";
+        response = true;
+        System.out.println(array[5]);
+
 
     
     }
