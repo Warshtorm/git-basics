f893afddf8ab06a4c141acfffbe277dcdf6b8396
Yura Bondarev <jary@inbox.ru>
2020-08-23 18:59:22 +0500

    change files Main and Questions

--- TestOFC/src/com/company/Main.java
+++ TestOFC/src/com/company/Main.java
@@ -1,38 +1,17 @@
 package com.company;
 
-import javax.swing.*;
-import java.awt.*;
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        boolean response;  // переменная для сравнения ответа
-        String[] array = new String[100];  //массив для вопросов
-        Scanner scan = new Scanner(System.in);  // ввод в консоль ответа
+
+    Questions ask = new Questions();
+
+    ask.zeroQuestion();
 
 
 
-        array[0] = "Длина струи пожарного ствола СПРК-8Б 25 метров.";
-        response = true;
-        System.out.println(array[0]);
-
-        array[1] = "Длина струи пожарного ствола СПРК-8Б 35 метров.";
-        response = false;
-        System.out.println(array[1]);
-        array[2] = "";
-        response = false;
-        System.out.println(array[2]);
-        array[3] = "";
-        response = true;
-        System.out.println(array[3]);
-        array[4] = "";
-        response = false;
-        System.out.println(array[4]);
-        array[5] = "";
-        response = true;
-        System.out.println(array[5]);
 
 
     
--- TestOFC/src/com/company/Questions.java
+++ TestOFC/src/com/company/Questions.java
@@ -1,12 +1,54 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Questions {
-    private boolean trueRequest = true;
-    private boolean falseRequest = false;
 
+    boolean response;  // переменная для сравнения ответа
+    String[] array = new String[10];  //массив для вопросов
+    Scanner scan = new Scanner(System.in);  // вызов сканнера
 
-    /*public boolean getRequest(int a, int b){
+    // через методы выводим вопросы под номерами
+    public void zeroQuestion() {
+        array[0] = "Длина струи пожарного ствола СПРК-8Б 25 метров.";
+        response = true;
+        System.out.println(array[0]);
+        scannerResponse();
+    }
 
-    }*/
+    public void firstQuestion() {
+        array[1] = "Длина струи пожарного ствола СПРК-8Б 35 метров.";
+        response = false;
+        System.out.println(array[1]);
+    }
 
-}
+    public void secondQuestion() {
+        array[2] = "";
+        response = false;
+        System.out.println(array[2]);
+    }
+
+    public void threeQuestion() {
+        array[3] = "";
+        response = true;
+        System.out.println(array[3]);
+    }
+
+    public void fourthQuestion() {
+        array[4] = "";
+        response = false;
+        System.out.println(array[4]);
+    }
+
+    public void fifthQuestion() {
+        array[5] = "";
+        response = true;
+        System.out.println(array[5]);
+    }
+
+    //метод для ввода ответа на вопрос
+    public void scannerResponse() {
+            System.out.println("Верно утверждение? Да - 1: Нет - 0 ");
+            int response = scan.nextInt();
+        }
+    }
