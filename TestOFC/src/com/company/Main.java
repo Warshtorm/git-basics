55f585353d376ec30eb432a45b9fbfe962e1e935
Yura Bondarev <jary@inbox.ru>
2020-08-24 20:19:31 +0500

    added cycles for exit and listing

--- TestOFC/src/com/company/Main.java
+++ TestOFC/src/com/company/Main.java
@@ -1,19 +1,36 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-    Questions ask = new Questions();
+        Questions ask = new Questions();
+        Questions get = new Questions();
+        Scanner scan = new Scanner(System.in);  // вызов сканнера
 
-    ask.zeroQuestion();
-    ask.fifthQuestion();
-    ask.secondQuestion();
+        int exit = 1;  // переменная для выхода
+
+        //  цикл, пока не нажмём ноль, программа работает
+        while (exit != 0) {
+            //  вывод 10 вопросов из списка
+            for (int cycle = 0; cycle < 5; cycle++) {
+                ask.zeroQuestion();
+
+                ask.fifthQuestion();
+                get.anAnswer();
+                ask.secondQuestion();
+                ask.threeQuestion();
+                ask.fourthQuestion();
+                ask.firstQuestion();
+            }
+
+            // проверка ввода, выход или продолжение программы
+            System.out.println("Продолжить: 1, Выход: 0.");
+            exit = scan.nextInt();
+        }
 
 
-
-
-
-    
     }
 }
--- TestOFC/src/com/company/Questions.java
+++ TestOFC/src/com/company/Questions.java
@@ -4,59 +4,53 @@ import java.util.Scanner;
 
 public class Questions {
 
-    boolean response;  // переменная для сравнения ответа
     String[] array = new String[10];  //массив для хранения вопросов
     Scanner scan = new Scanner(System.in);  // вызов сканнера
 
-    public Questions(){
-        array[0] = "Длина струи пожарного ствола СПРК-8Б 25 метров.";
-        array[1] = "Длина струи пожарного ствола СПРК-8Б 35 метров.";
-        array[2] = "Вопрос";
-        array[3] = "Вопрос";
-        array[4] = "Вопрос";
-        array[5] = "Вопрос";
+
+
+    public Questions() {
+        array[0] = "Длина струи пожарного ствола СПРК-8Б - 25 метров.";
+        array[1] = "Длина струи пожарного ствола СПРК-8Б - 35 метров.";
+        array[2] = "Производительность ГПС-600 составляет - 600 л.с.";
+        array[3] = "Производительность ГПС-600 составляет - 500 л.с.";
+        array[4] = "СПРК - ствол пожарный, ручной, комбинированный";
+        array[5] = "СПРК - ствол пожарный, разной кратности";
+        array[6] = "СПРК - ствол пожарный, разной кратности";
+        array[7] = "СПРК - ствол пожарный, разной кратности";
+        array[8] = "СПРК - ствол пожарный, разной кратности";
+        array[9] = "СПРК - ствол пожарный, разной кратности";
     }
 
     // через методы выводим вопросы под номерами
     public void zeroQuestion() {
-        System.out.println(array[0]);
-        response = true;
-        scannerResponse();
+        System.out.println("Верно утверждение? Да - 1: Нет - 0 " + array[0]);
+        
+
     }
 
     public void firstQuestion() {
         System.out.println(array[1]);
-        response = false;
-        scannerResponse();
     }
 
     public void secondQuestion() {
         System.out.println(array[2]);
-        response = false;
-        scannerResponse();
     }
 
     public void threeQuestion() {
         System.out.println(array[3]);
-        response = true;
-        scannerResponse();
     }
 
     public void fourthQuestion() {
         System.out.println(array[4]);
-        response = false;
-        scannerResponse();
     }
 
     public void fifthQuestion() {
         System.out.println(array[5]);
-        response = true;
-        scannerResponse();
     }
 
-    //метод для ввода ответа на вопрос
-    public void scannerResponse() {
-            System.out.println("Верно утверждение? Да - 1: Нет - 0 ");
-            int response = scan.nextInt();
-        }
+    // вводим ответ в консоль
+    public  void anAnswer(){
+
     }
+}
